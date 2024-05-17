50dd9143cf50a01972f76c29405a7d07b04ee7cb
Silmunia <62479229+Silmunia@users.noreply.github.com>
2024-05-17 06:57:20 -0300

    Fix issue with Pokemon names longer than 10 chars

--- odin-memory/src/components/Card.jsx
+++ odin-memory/src/components/Card.jsx
@@ -5,12 +5,20 @@ function Card({ identifier, trigger, name, image }) {
 
     const uppercaseName = name.charAt(0).toUpperCase() + name.slice(1);
 
+    let parsedName = uppercaseName;
+
+    if (parsedName.length > 10) {
+        const dashIndex = parsedName.indexOf('-');
+
+        parsedName = parsedName.slice(0, dashIndex);
+    }
+
     return (
         <div onClick={() => trigger(identifier)}
             className="card" id={identifier}
         >
             <img src={image}></img>
-            <h3>{uppercaseName}</h3>
+            <h3>{parsedName}</h3>
         </div>
     )
 }
